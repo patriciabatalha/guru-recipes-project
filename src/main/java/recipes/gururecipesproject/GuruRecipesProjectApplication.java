588052928d0e556e824afb467b16dce82ff41435
Patricia Batalha <patibatalha@gmail.com>
2021-02-24 16:56:01 -0300

    Initial commit

---
+++ src/main/java/recipes/gururecipesproject/GuruRecipesProjectApplication.java
@@ -0,0 +1,13 @@
+package recipes.gururecipesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruRecipesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruRecipesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/recipes/gururecipesproject/GuruRecipesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package recipes.gururecipesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruRecipesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
