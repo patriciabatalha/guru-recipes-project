9827e44ade867ff88bda0850d7d364b1610cc1f7
Patricia Batalha <patibatalha@gmail.com>
2021-03-14 19:22:38 -0300

    Completing assignment.

---
+++ src/main/java/recipes/gururecipesproject/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package recipes.gururecipesproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        System.out.println("Some message to say... 1234567");
+        return "index";
+    }
+
+}
